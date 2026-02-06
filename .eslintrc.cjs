602917610a55c87ce33c81ff9b5522b967ed113a
Muhamed-Ragab <mohamedragab1649@gmail.com>
2026-02-06 17:42:07 +0200

    chore(deps+scripts): remove vitest & bcryptjs, switch to node --watch, drop docker-compose
    - remove vitest, tests and vitest.config.js\n- remove bcryptjs and nodemon\n- change dev script to use node --watch\n- remove docker-compose.yml (docker support removed)\n- add minimal ESLint config for CI

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2021: true,
+    jest: false,
+  },
+  extends: ['eslint:recommended', 'plugin:node/recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'no-console': 'off'
+  }
+};
