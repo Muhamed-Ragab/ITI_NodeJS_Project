643ed55fe7934be0360a10c2a95ac86a777cf064
abdalrahmanalirajab <moaliabody@gmail.com>
2026-02-16 23:39:16 +0200

    run the npm format

--- src/modules/categories/categories.service.js
+++ src/modules/categories/categories.service.js
@@ -1,50 +1,48 @@
 import * as categoryRepository from "./categories.repository.js";
 import slugify from "slugify";
 
- // Create Category
- 
+// Create Category
+
 export const createCategory = async (category) => {
-    const slug = slugify(category.name, {
-        lower: true,
-        strict: true
-    });
+	const slug = slugify(category.name, {
+		lower: true,
+		strict: true,
+	});
 
-    return await categoryRepository.create({
-        ...category,
-        slug
-    });
+	return await categoryRepository.create({
+		...category,
+		slug,
+	});
 };
 
- // Get Category By ID
- 
+// Get Category By ID
+
 export const getCategoryById = async (id) => {
-    return await categoryRepository.findById(id);
+	return await categoryRepository.findById(id);
 };
 
- //Update Category
- 
+//Update Category
+
 export const updateCategory = async (id, category) => {
+	// Only update slug if name exists
+	if (category.name) {
+		category.slug = slugify(category.name, {
+			lower: true,
+			strict: true,
+		});
+	}
 
-    // Only update slug if name exists
-    if (category.name) {
-        category.slug = slugify(category.name, {
-            lower: true,
-            strict: true
-        });
-    }
-
-    return await categoryRepository.updateById(id, category);
+	return await categoryRepository.updateById(id, category);
 };
 
- // Delete Category
- 
+// Delete Category
+
 export const deleteCategory = async (id) => {
-    return await categoryRepository.deleteById(id);
+	return await categoryRepository.deleteById(id);
 };
 
+// List Categories
 
- // List Categories
- 
 export const listCategories = async () => {
-    return await categoryRepository.list();
+	return await categoryRepository.list();
 };
