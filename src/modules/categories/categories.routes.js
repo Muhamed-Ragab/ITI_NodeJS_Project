f4167b36a930af67e0e6a9b5c03cf87b737a6c6a
Muhamed-Ragab <mohamedragab1649@gmail.com>
2026-02-17 05:10:29 +0200

    refactor(categories): rename router to categoryRouter and remove legacy example tests

--- src/modules/categories/categories.routes.js
+++ src/modules/categories/categories.routes.js
@@ -4,40 +4,40 @@ import { requireRole } from "../../middlewares/role.middleware.js";
 import { validate } from "../../middlewares/validate.middleware.js";
 import * as categoryController from "./categories.controller.js";
 import {
-	categoryCreateSchema,
-	categoryIdSchema,
-	categoryUpdateSchema,
+  categoryCreateSchema,
+  categoryIdSchema,
+  categoryUpdateSchema,
 } from "./categories.validation.js";
 
-const router = Router();
+const categoryRouter = Router();
 
-router
-	.route("/")
-	.get(categoryController.listCategories)
-	.post(
-		requireAuth,
-		requireRole("admin"),
-		validate({ body: categoryCreateSchema }),
-		categoryController.createCategory
-	);
+categoryRouter
+  .route("/")
+  .get(categoryController.listCategories)
+  .post(
+    requireAuth,
+    requireRole("admin"),
+    validate({ body: categoryCreateSchema }),
+    categoryController.createCategory,
+  );
 
-router
-	.route("/:id")
-	.get(
-		validate({ params: categoryIdSchema }),
-		categoryController.getCategoryById
-	)
-	.put(
-		requireAuth,
-		requireRole("admin"),
-		validate({ params: categoryIdSchema, body: categoryUpdateSchema }),
-		categoryController.updateCategory
-	)
-	.delete(
-		requireAuth,
-		requireRole("admin"),
-		validate({ params: categoryIdSchema }),
-		categoryController.deleteCategory
-	);
+categoryRouter
+  .route("/:id")
+  .get(
+    validate({ params: categoryIdSchema }),
+    categoryController.getCategoryById,
+  )
+  .put(
+    requireAuth,
+    requireRole("admin"),
+    validate({ params: categoryIdSchema, body: categoryUpdateSchema }),
+    categoryController.updateCategory,
+  )
+  .delete(
+    requireAuth,
+    requireRole("admin"),
+    validate({ params: categoryIdSchema }),
+    categoryController.deleteCategory,
+  );
 
-export default router;
+export default categoryRouter;
