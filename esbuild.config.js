b5ef2d754d66e1af8af63dc5b495bd47e989ed4a
Muhamed-Ragab <mohamedragab1649@gmail.com>
2026-02-23 10:24:48 +0200

    style: format esbuild.config.js

--- esbuild.config.js
+++ esbuild.config.js
@@ -3,35 +3,35 @@ import * as esbuild from "esbuild";
 const isWatch = process.argv.includes("--watch");
 
 const config = {
-  entryPoints: ["src/server.js"],
-  bundle: true,
-  platform: "node",
-  format: "esm",
-  target: "node20",
-  outfile: "dist/server.js",
-  minify: true,
-  external: [
-    "bcryptjs",
-    "cloudinary",
-    "cors",
-    "dotenv",
-    "express",
-    "http-status-codes",
-    "jsonwebtoken",
-    "mongoose",
-    "morgan",
-    "nodemailer",
-    "slugify",
-    "stripe",
-    "zod",
-  ],
+	entryPoints: ["src/server.js"],
+	bundle: true,
+	platform: "node",
+	format: "esm",
+	target: "node20",
+	outfile: "dist/server.js",
+	minify: true,
+	external: [
+		"bcryptjs",
+		"cloudinary",
+		"cors",
+		"dotenv",
+		"express",
+		"http-status-codes",
+		"jsonwebtoken",
+		"mongoose",
+		"morgan",
+		"nodemailer",
+		"slugify",
+		"stripe",
+		"zod",
+	],
 };
 
 if (isWatch) {
-  const ctx = await esbuild.context(config);
-  await ctx.watch();
-  console.log("Watching for changes...");
+	const ctx = await esbuild.context(config);
+	await ctx.watch();
+	console.log("Watching for changes...");
 } else {
-  await esbuild.build(config);
-  console.log("Build complete!");
+	await esbuild.build(config);
+	console.log("Build complete!");
 }
